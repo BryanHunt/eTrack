2d07f853edb180f5e1b4fd7b1d88bf33dfac1a83
bhunt <bhunt@nvidia.com>
2012-03-27 22:41:05 -0500

    Initial checkin

---
+++ experimental/org.eclipse.etrack.web.project/src/org/eclipse/etrack/web/project/resources/ProjectResource.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Bryan Hunt.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bryan Hunt - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.etrack.web.project.resources;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipselabs.etrack.web.storage.resources.StorageResource;
+
+/**
+ * @author bhunt
+ * 
+ */
+public class ProjectResource extends StorageResource<EObject>
+{
+	@Override
+	protected void mapStorageURI(Map<URI, URI> uriMap)
+	{
+		// FIXME : determine the mapping dynamically from the ResourceProvider
+		URI logicalURI = URI.createURI(System.getProperty("baseURI", "http://localhost:8080")).appendSegments(new String[] { "storage", "projects" });
+		URI physicalURI = URI.createURI(System.getProperty("mongodb", "mongo://localhost/etrack")).appendSegment("projects");
+		uriMap.put(logicalURI, physicalURI);
+	}
+}
