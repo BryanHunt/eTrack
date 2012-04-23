b968fd6bc03650b60c692ab11ee169f37da839e1
Bryan Hunt <bhunt@mac.com>
2012-04-22 21:19:41 -0500

    Initial checkin

---
+++ experimental/org.eclipselabs.etrack.web.task/src/org/eclipselabs/etrack/web/task/providers/ConnectionValidationResourceProvider.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Bryan Hunt.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bryan Hunt - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipselabs.etrack.web.task.providers;
+
+import org.eclipselabs.etrack.web.task.resources.ConnectionValidationResource;
+import org.restlet.Context;
+import org.restlet.ext.osgi.ResourceProvider;
+import org.restlet.resource.Finder;
+
+/**
+ * @author bhunt
+ * 
+ */
+public class ConnectionValidationResourceProvider extends ResourceProvider
+{
+	@Override
+	protected Finder createFinder(Context context)
+	{
+		return new Finder(context, ConnectionValidationResource.class);
+	}
+}
