954ca2765f52f19c2dcbd3af8465ae39c3d0b6c1
bhunt <bhunt@nvidia.com>
2012-05-01 20:34:19 -0500

    Add the Mylyn repository listener after the workbench has started.

--- client/org.eclipselabs.etrack.client.web.mylyn/src/org/eclipselabs/etrack/client/web/mylyn/MylynPasswordCredentialProviderManager.java
+++ client/org.eclipselabs.etrack.client.web.mylyn/src/org/eclipselabs/etrack/client/web/mylyn/MylynPasswordCredentialProviderManager.java
@@ -8,8 +8,10 @@ package org.eclipselabs.etrack.client.web.mylyn;
 import java.util.HashMap;
 
 import org.eclipse.mylyn.tasks.core.IRepositoryListener;
+import org.eclipse.mylyn.tasks.core.IRepositoryManager;
 import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.ui.TasksUi;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * @author bhunt
@@ -20,19 +22,51 @@ public class MylynPasswordCredentialProviderManager implements IRepositoryListen
 	public MylynPasswordCredentialProviderManager()
 	{
 		providers = new HashMap<String, MylynPasswordCredentialProvider>();
-		TasksUi.getRepositoryManager().addListener(this);
+
+		new Thread()
+		{
+			@Override
+			public void run()
+			{
+				while (!PlatformUI.isWorkbenchRunning())
+				{
+					try
+					{
+						sleep(1000);
+					}
+					catch (InterruptedException e)
+					{}
+				}
+
+				synchronized (providers)
+				{
+					IRepositoryManager repositoryManager = TasksUi.getRepositoryManager();
+					repositoryManager.addListener(MylynPasswordCredentialProviderManager.this);
+
+					for (TaskRepository repository : repositoryManager.getAllRepositories())
+						repositoryAdded(repository);
+				}
+			}
+		}.start();
 	}
 
 	@Override
 	public void repositoryAdded(TaskRepository repository)
 	{
-		providers.put(repository.getRepositoryUrl(), MylynPasswordCredentialProvider.buildProvider(repository));
+		synchronized (providers)
+		{
+			if (!providers.containsKey(repository.getRepositoryUrl()))
+				providers.put(repository.getRepositoryUrl(), MylynPasswordCredentialProvider.buildProvider(repository));
+		}
 	}
 
 	@Override
 	public void repositoryRemoved(TaskRepository repository)
 	{
-		providers.get(repository.getRepositoryUrl()).dispose();
+		synchronized (providers)
+		{
+			providers.get(repository.getRepositoryUrl()).dispose();
+		}
 	}
 
 	@Override
@@ -42,8 +76,11 @@ public class MylynPasswordCredentialProviderManager implements IRepositoryListen
 	@Override
 	public void repositoryUrlChanged(TaskRepository repository, String oldUrl)
 	{
-		providers.remove(oldUrl).dispose();
-		repositoryAdded(repository);
+		synchronized (providers)
+		{
+			providers.remove(oldUrl).dispose();
+			repositoryAdded(repository);
+		}
 	}
 
 	private HashMap<String, MylynPasswordCredentialProvider> providers;
