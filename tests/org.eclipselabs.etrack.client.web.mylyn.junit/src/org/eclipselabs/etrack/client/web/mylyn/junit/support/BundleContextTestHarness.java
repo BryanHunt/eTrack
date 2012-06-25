4cb12bde5bc92b09d9b4c78c878771c89bd38b00
Bryan Hunt <bhunt@mac.com>
2012-06-24 20:22:15 -0500

    Fix header copyright

--- tests/org.eclipselabs.etrack.client.web.mylyn.junit/src/org/eclipselabs/etrack/client/web/mylyn/junit/support/BundleContextTestHarness.java
+++ tests/org.eclipselabs.etrack.client.web.mylyn.junit/src/org/eclipselabs/etrack/client/web/mylyn/junit/support/BundleContextTestHarness.java
@@ -1,6 +1,13 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2012 Bryan Hunt.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Bryan Hunt - initial API and implementation
+ *******************************************************************************/
 
 package org.eclipselabs.etrack.client.web.mylyn.junit.support;
 
@@ -37,105 +44,76 @@ public class BundleContextTestHarness implements BundleContext
 	@Override
 	public String getProperty(String key)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bundle getBundle()
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bundle installBundle(String location, InputStream input) throws BundleException
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bundle installBundle(String location) throws BundleException
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bundle getBundle(long id)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bundle[] getBundles()
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void addServiceListener(ServiceListener listener, String filter) throws InvalidSyntaxException
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public void addServiceListener(ServiceListener listener)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public void removeServiceListener(ServiceListener listener)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public void addBundleListener(BundleListener listener)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public void removeBundleListener(BundleListener listener)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public void addFrameworkListener(FrameworkListener listener)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public void removeFrameworkListener(FrameworkListener listener)
-	{
-		// TODO Auto-generated method stub
-
-	}
+	{}
 
 	@Override
 	public ServiceRegistration<?> registerService(String[] clazzes, Object service, Dictionary<String, ?> properties)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ServiceRegistration<?> registerService(String clazz, Object service, Dictionary<String, ?> properties)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -149,71 +127,60 @@ public class BundleContextTestHarness implements BundleContext
 	@Override
 	public ServiceReference<?>[] getServiceReferences(String clazz, String filter) throws InvalidSyntaxException
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ServiceReference<?>[] getAllServiceReferences(String clazz, String filter) throws InvalidSyntaxException
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ServiceReference<?> getServiceReference(String clazz)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public <S> ServiceReference<S> getServiceReference(Class<S> clazz)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public <S> Collection<ServiceReference<S>> getServiceReferences(Class<S> clazz, String filter) throws InvalidSyntaxException
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public <S> S getService(ServiceReference<S> reference)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean ungetService(ServiceReference<?> reference)
 	{
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public File getDataFile(String filename)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Filter createFilter(String filter) throws InvalidSyntaxException
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Bundle getBundle(String location)
 	{
-		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
