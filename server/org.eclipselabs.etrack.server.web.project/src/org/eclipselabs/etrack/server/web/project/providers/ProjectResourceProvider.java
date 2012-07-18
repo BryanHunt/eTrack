069c71e988870e4264046b467a1d70731c42b9a5
Bryan Hunt <bhunt@nvidia.com>
2012-07-18 14:56:48 -0500

    Added project resource

---
+++ server/org.eclipselabs.etrack.server.web.project/src/org/eclipselabs/etrack/server/web/project/providers/ProjectResourceProvider.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 NVIDIA CORPORATION.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    NVIDIA - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipselabs.etrack.server.web.project.providers;
+
+import org.eclipselabs.etrack.server.web.project.resources.ProjectResource;
+import org.eclipselabs.etrack.server.web.storage.AbstractStorageResourceProvider;
+import org.restlet.Context;
+import org.restlet.resource.Finder;
+
+/**
+ * @author bhunt
+ * 
+ */
+public class ProjectResourceProvider extends AbstractStorageResourceProvider
+{
+	@Override
+	protected Finder createFinder(Context context)
+	{
+		return new Finder(context, ProjectResource.class);
+	}
+}
